3020c637e23665e6f25b0f382081931d76ac26af
GLDHASINI <2420030421@klh.edu.in>
2025-07-26 09:17:29 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,12 @@
+package oops_practical;
+
+public class ArithmeticOperators {
+public static void main(String[] args) {
+	int a=5, b=7;
+	System.out.println("Addition: " + (a + b));
+	System.out.println("Subtraction: " + (a - b));
+	System.out.println("Multiplication: " + (a*b));
+	System.out.println("Division: " + (a / b));
+	System.out.println("Modulus: " + (a % b));
+}
+}
